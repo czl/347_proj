808b5d6939772b25debfd616f676f9c44377d714
czl <clemenslee@utexas.edu>
2015-04-19 19:15:13 -0400

    working on parsing neo4j json, page is loading fine, not just to parse and inject html

--- dash.js
+++ dash.js
@@ -4,7 +4,7 @@ function pull_user_name(){
     url: 'session.php',
     dataType: 'text',
     success: function(status){
-      console.log("raw status: " + status);
+//      console.log("raw status: " + status);
       split_ray = status.split("-");
       if(split_ray[0] == "no session"){
 	window.location = "http://staycalm.me";
@@ -23,16 +23,17 @@ function pull_user_name(){
     }
   }).done(function(status){
     if(status != '' && status.split("-").length ==3){
-      console.log("if condition met");
+//      console.log("if condition met");
       get_recommended_events(status.split("-")[1]);
     }
-  });//end ajax
+    $('body').css('display','block');
+  });//end done clause 
 }
 function get_recommended_events(username){
   var query= "MATCH (n:user{username:'"+username+"'}),(t1:tag{tag:n.like1}), (t2:tag{tag:n.like2}), (t3:tag{tag:n.like3}) OPTIONAL MATCH (e1)-[:tag]->(t1) OPTIONAL MATCH (e2)-[:tag]->(t2) OPTIONAL MATCH (e3)-[:tag]->(t3) return e1,e2,e3";
-  console.log("query: " + query);
-console.log("stringify");
-console.log(JSON.stringify({"statements":[{"statement":query}]}));
+//  console.log("query: " + query);
+//  console.log("stringify");
+//  console.log(JSON.stringify({"statements":[{"statement":query}]}));
   $.ajax({//ajax to pull recommended events
     type: "POST",
     url: "http://104.131.68.36:7474/db/data/transaction",
@@ -52,6 +53,11 @@ console.log(JSON.stringify({"statements":[{"statement":query}]}));
     success: function(status){
       console.log("recommended events return: ");
       console.log(status);
+      console.log("printed recommended events json");
+      console.log(status.results[0].data);
+//      var rec_events = 
+//      $('#user_name').html("Welcome, "+user_name);
+
     },
     error: function(status){
       alert("error for recommended events");
@@ -62,11 +68,8 @@ console.log(JSON.stringify({"statements":[{"statement":query}]}));
 }
 function pre_load(){
   pull_user_name();
-  console.log("pre_load done");
-}
-$.when(pre_load()).done(function(){
-  $('body').css('display','block'); 
-}); 
+}pre_load();
+
 $(document).ready(function(){
   $("#logout_btn").click(function(){
     console.log("logout button clicked");
