fbf7f687c044415f95bdab9b4b47757323f3678b
czl <clemenslee@utexas.edu>
2015-04-20 17:19:21 -0400

    added find follows and fixed up html a bit

--- dash.js
+++ dash.js
@@ -24,7 +24,9 @@ function pull_user_name(){
   }).done(function(status){
     if(status != '' && status.split("-").length ==3){
 //      console.log("if condition met");
-      get_recommended_events(status.split("-")[1]);
+      var username = status.split("-")[1];
+      get_recommended_events(username);
+      get_follow(username);
     }
     $('body').css('display','block');
   });//end done clause 
@@ -73,6 +75,52 @@ function get_recommended_events(username){
     }
   });
 }
+
+function get_follow(username){
+  var query= "MATCH (n:user{username:'"+username+"'})-[:follow]-(m:user)return DISTINCT(m)";
+  console.log("query: " + query);
+//  console.log("stringify");
+//  console.log(JSON.stringify({"statements":[{"statement":query}]}));
+  $.ajax({//ajax to pull recommended events
+    type: "POST",
+    url: "http://104.131.68.36:7474/db/data/transaction",
+    accepts: "application/json",
+    dataType: "json",
+    contentType: "application/json",
+    crossDomain: true,
+    "X-Stream": "true",
+    headers:{
+      "Authorization": "Basic bmVvNGo6Y2xlbWVucw=="
+    },  
+    data: JSON.stringify({
+      "statements": [{
+        "statement": query
+      }] 
+    }), 
+    success: function(status){
+      console.log("follow return: ");
+      console.log(status);
+      console.log("printed follow json");
+      var data_result = status.results[0].data;
+      console.log(data_result);
+      for(i=0;i<data_result.length;i++){
+        console.log(data_result[i].row[0].first);
+        console.log(data_result[i].row[0].last);
+//        console.log(data_result[i].row[0].time);
+//        console.log(data_result[i].row[0].date_start);
+//        console.log(data_result[i].row[0].date_end);
+
+      }
+      html_follow(data_result); 
+    },
+    error: function(status){
+      alert("error for recommended events");
+      console.log("error: ");
+      console.log(status);
+    }
+  });
+}
+
 function pre_load(){
   pull_user_name();
 }pre_load();
@@ -96,6 +144,30 @@ function html_recommended_events(data){
   }
   console.log(html_build);
   $('#recommended_events_body').html(html_build);
+}
+
+function html_follow(data){
+  var html_build = '';
+  for(i=0;i<data.length;i++){
+    html_build += '<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title">';
+    html_build += data[i].row[0].first + "&nbsp";
+    html_build += data[i].row[0].last;
+    html_build += '</h4></div><div class="panel-body">';
+    html_build += 'Likes: ';
+    html_build += '<ul class="list-group">';
+    html_build += '<li class="list-group-item">'+ data[i].row[0].like1 +'</li>';
+    html_build += '<li class="list-group-item">'+ data[i].row[0].like2 +'</li>';
+    html_build += '<li class="list-group-item">'+ data[i].row[0].like3 +'</li>';
+    html_build += '</div></div>';
+/*<div class="panel panel-default">
+  <div class="panel-heading"><h4 class="panel-title">subpanel1 title</h4></div>
+  <div class="panel-body">subpanel1 body</div>
+</div>
+*/
+
+  }
+  console.log(html_build);
+  $('#follow_body').html(html_build);
 }
 
 
