b3391e0207be6a2ef9fcade0c3759cc0e4e8f2ab
czl <clemenslee@utexas.edu>
2015-04-22 12:10:33 -0400

    got beginning of RESTful service set up... need to figure out how to get AJAX params to send correctly, was sending via json.stringify, but didn't work. only works when sending via text

---
+++ REST/test_REST.php
@@ -0,0 +1,11 @@
+<?php
+
+//echo "hello test REST";
+
+//$method = $_POST['method'];
+//print_r($_POST);
+//echo $method;
+$ret_arr = array('ret'=>"end");
+echo "hello";
+echo $_POST['a'];
+echo json_encode($_POST);
