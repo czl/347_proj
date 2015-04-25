463418b50dd8a147de9d89e456ba939934c173e6
czl <clemenslee@utexas.edu>
2015-04-25 00:54:21 -0400

    clear out old get follows function in dash.js

--- dash.js
+++ dash.js
@@ -123,54 +123,6 @@ function get_follows(username){
                         console.log(status);
                 }
         });
-  
-}
-/**
-no longer to be used, this has been replaced by html creation on server side
-*/
-function get_follow_dicontinued(username){
-  var query= "MATCH (n:user{username:'"+username+"'})-[:follow]-(m:user)return DISTINCT(m)";
-  console.log("query: " + query);
-//  console.log("stringify");
-//  console.log(JSON.stringify({"statements":[{"statement":query}]}));
-  $.ajax({//ajax to pull recommended events
-    type: "POST",
-    url: "http://104.131.68.36:7474/db/data/transaction",
-    accepts: "application/json",
-    dataType: "json",
-    contentType: "application/json",
-    crossDomain: true,
-    "X-Stream": "true",
-    headers:{
-      "Authorization": "Basic bmVvNGo6Y2xlbWVucw=="
-    },  
-    data: JSON.stringify({
-      "statements": [{
-        "statement": query
-      }] 
-    }), 
-    success: function(status){
-      console.log("follow return: ");
-      console.log(status);
-      console.log("printed follow json");
-      var data_result = status.results[0].data;
-      console.log(data_result);
-      for(i=0;i<data_result.length;i++){
-        console.log(data_result[i].row[0].first);
-        console.log(data_result[i].row[0].last);
-//        console.log(data_result[i].row[0].time);
-//        console.log(data_result[i].row[0].date_start);
-//        console.log(data_result[i].row[0].date_end);
-
-      }
-      html_follow(data_result); 
-    },
-    error: function(status){
-      alert("error for recommended events");
-      console.log("error: ");
-      console.log(status);
-    }
-  });
 }
 
 function pre_load(){
@@ -201,46 +153,6 @@ function html_recommended_events(data){
   console.log(html_build);
   $('#recommended_events_body').html(html_build);
 }
-
-function html_follow(data){
-  var html_build = '';
-  for(i=0;i<data.length;i++){
-    html_build += '<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title">';
-    html_build += data[i].row[0].first + "&nbsp";
-    html_build += data[i].row[0].last;
-    html_build += '</h4></div><div class="panel-body">';
-    html_build += 'Likes: ';
-    html_build += '<ul class="list-group">';
-    html_build += '<li class="list-group-item">'+ data[i].row[0].like1 +'</li>';
-    html_build += '<li class="list-group-item">'+ data[i].row[0].like2 +'</li>';
-    html_build += '<li class="list-group-item">'+ data[i].row[0].like3 +'</li>';
-    html_build += '</ul>';
-    html_build += '</div></div>';
-/*<div class="panel panel-default">
-  <div class="panel-heading"><h4 class="panel-title">subpanel1 title</h4></div>
-  <div class="panel-body">subpanel1 body</div>
-</div>
-*/
-
-  }
-  console.log(html_build);
-  $('#follow_body').html(html_build);
-///////////////start build left panel
-  html_build = '';
-  html_build += '<ul class="list-group">';
-  for(i=0;i<data.length;i++){
-/*    html_build += '<li id="'+data[i].row[0].username+'" '+
-                  'class="list-group-item">'+ data[i].row[0].first +
-                  '&nbsp'+data[i].row[0].last+'</li>'; 
-*/
-    html_build += '<li class="list-group-item hover_name">'+ data[i].row[0].first +
-                  '&nbsp'+data[i].row[0].last+'</li>'; 
-  }
-  console.log(html_build);
-  $('#follow_left_panel').html(html_build);
- 
-}
-
 
 $(document).ready(function(){
   $("#logout_btn").click(function(){
